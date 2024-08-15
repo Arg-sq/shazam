2f38c3dd4be3e8d0017b88a037c339ccdbcc244b
ashesh <ashesh7272@gmail.com>
2024-08-15 23:01:03 +0545

    visit past

--- app/index.tsx
+++ app/index.tsx
@@ -4,3 +4,6 @@ const Home = () => {
   return <Redirect href="/(auth)/welcome" />;
 };
 export default Home;
+
+//check old commit msg try
+// git commit --date="YYYY-MM-DD HH:MM:SS" -m "Your commit message"
