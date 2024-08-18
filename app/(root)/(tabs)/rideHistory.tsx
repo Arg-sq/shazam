2490956d188495382359a9186e9ace36bafa3d15
ashesh <ashesh7272@gmail.com>
2024-08-18 19:13:39 +0545

    feat: added new routes and pages

---
+++ app/(root)/(tabs)/rideHistory.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const rideHistory = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hosdsddme</Text>
+    </SafeAreaView>
+  );
+};
+export default rideHistory;
