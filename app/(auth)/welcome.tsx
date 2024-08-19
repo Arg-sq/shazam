af20f7f566400d4c95a6300ea66327f8b1f6f233
ashesh <ashesh7272@gmail.com>
2024-08-19 15:33:31 +0545

    Feat: add custom button and splash image components

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,11 +1,15 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { router } from "expo-router";
 import Swiper from "react-native-swiper";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { onboarding } from "@/constants";
+import CustomButton from "../components/CustomButton";
 
 const OnBoarding = () => {
   const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const isLastSlide = activeIndex === onboarding.length - 1;
   return (
     <SafeAreaView className="flex h-full items-center justify-between">
       <TouchableOpacity
@@ -20,9 +24,46 @@ const OnBoarding = () => {
       <Swiper
         ref={swiperRef}
         loop={false}
-        dot={<View className="w-[32px] h-[4px] mx-1 bg-primary-200" />}
-        activeDot={<View className="w-[32px] h-[4px] mx-1 bg-primary-500" />}
-      ></Swiper>
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-primary-400 rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-primary-500 rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding?.map((item) => {
+          return (
+            <View
+              key={item.id}
+              className="flex items-center justify-center p-5"
+            >
+              <Image
+                source={item.image}
+                className="w-full h-[300px]"
+                resizeMode="contain"
+              />
+              <View className="flex flex-row items-center justify-center">
+                <Text className="text-black text-3xl font-bold mx-10 text-center">
+                  {item.title}
+                </Text>
+              </View>
+              <Text className="text-lg font-JakartaSemiBold text-center text-slate-800 mx-10 mt-3">
+                {item.description}
+              </Text>
+            </View>
+          );
+        })}
+      </Swiper>
+      <CustomButton
+        title={isLastSlide ? "Get Started" : "Next"}
+        onPress={() =>
+          isLastSlide
+            ? router.replace("/(auth)/sign-up")
+            : swiperRef.current?.scrollBy(1)
+        }
+        className="w-11/12 mt-10"
+      />
     </SafeAreaView>
   );
 };
---
+++ app/components/CustomButton.tsx
@@ -0,0 +1,46 @@
+import { ButtonProps } from "@/types/type";
+import { Text, TouchableOpacity } from "react-native";
+
+const getBgVariantStyle: {
+  [key in NonNullable<ButtonProps["bgVariant"]>]: string;
+} = {
+  primary: "bg-[#0286ff]",
+  secondary: "bg-gray-500",
+  danger: "bg-red-500",
+  success: "bg-green-500",
+  outline: "bg-transparent border-neutral-300 border-[0.5px]",
+};
+
+const getTextVariantStyle: {
+  [key in NonNullable<ButtonProps["textVariant"]>]: string;
+} = {
+  primary: "text-black",
+  secondary: "text-gray-100",
+  danger: "text-red-100",
+  success: "text-green-100",
+};
+
+const CustomButton = ({
+  onPress,
+  title,
+  bgVariant = "primary",
+  textVariant = "default",
+  IconLeft,
+  IconRight,
+  className,
+  ...props
+}: ButtonProps) => (
+  <TouchableOpacity
+    className={`w-11/12 py-2 rounded-full flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle[bgVariant]} ${className}`}
+    onPress={onPress}
+  >
+    {IconLeft && <IconLeft />}
+    <Text
+      className={`text-lg font-bold ${getTextVariantStyle[textVariant] || "text-white"}`}
+    >
+      {title}
+    </Text>
+    {IconRight && <IconRight />}
+  </TouchableOpacity>
+);
+export default CustomButton;
